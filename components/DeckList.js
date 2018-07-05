00f76e7691bef4d332c56aab5aadad305b086c7a
Christian Cadieux <cadieux_christian@hotmail.com>
2018-07-05 13:58:03 -0400

    feat: DeckList properly sending title and cardNum to DeckItem

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableHighlight } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 import { getDecks } from '../utils/api.js';
 import { receiveDecks } from '../actions';
 import { connect } from 'react-redux';
 import { AppLoading} from 'expo';
+
 
 class DeckList extends Component {
 	state = {
@@ -18,18 +20,40 @@ class DeckList extends Component {
 		.then(function(decks){
 			dispatch(receiveDecks(decks))
 		})
+		.then(() => this.setState({ready: true}))
 		.catch(function(err){
 			console.error('There was an error retreiving decks from AsyncStorage:', err)
 		})
-
 	}
 
+	renderItem = ({ item }) => {
+		return (
+			<TouchableHighlight style={styles.item}
+				onPress={() => this.props.navigation.navigate(
+					'DeckItem',
+					{title: item.title, cardNum: item.cardNum}
+				)}
+			>
+				<View>
+					<Text style={styles.title}  >{item.title}</Text>
+					<Text style={styles.card} >{`${item.cardNum} cards`}</Text>
+				</View>
+			</TouchableHighlight>
+		)
+	}
 	render(){
 		const { ready } = this.state
+		if (ready === false) {
+      return <AppLoading />
+    }
 
 		return (
 			<View style={styles.container}>
-				<Text style={styles.title}>This is DeckList.js</Text>
+				<FlatList
+					data={this.props.decks}
+					renderItem={this.renderItem}
+					keyExtractor={(item, index) => item.title}
+				/>
 			</View>
 		)
 	}
@@ -38,18 +62,39 @@ class DeckList extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-
+	},
+	item: {
+		flex: 1,
+		paddingTop: 20,
+		paddingBottom: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderBottomWidth: 2,
+	},
+	card: {
+		textAlign: 'center',
+		color: 'rgb(60,60,60)'
 	},
 	title: {
-		textAlign: 'center',
+		fontSize: 30,
 
 	}
 })
 
 
-function mapStateToProps (decks) {
+function mapStateToProps (state) {
+	let deckArray;
+	if (Object.keys(state).length !== 0 ){
+		deckArray = Object.keys(state).map((deck) => {
+			let title = state[deck].title
+			let cardNum = state[deck].questions.length
+			return {title, cardNum}
+		})
+	} else {
+		deckArray = {}
+	}
 	return {
-		decks
+		decks: deckArray
 	}
 }
-export default connect(mapStateToProps)(DeckList);
+export default connect(mapStateToProps)(DeckList)
